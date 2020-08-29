6b36efbb4d2ad77b15629a6508ff3c3d77c5b9e4
SinhaUjj <ujjavaljolly@gmail.com>
2020-08-30 03:15:28 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import styles from './App.module.css'
 import { fetchRecipes } from './api'
 import {Recipe } from './components/Recipe/Recipe';
 import food from './images/transparent.png'
+import spinner from './images/spinner.svg'
+
 function App() {
-  const [ recipes, setRecipes ] = useState([])
+  const [ recipes, setRecipes ] = useState(['first'])
   const [ dish, setDish ] = useState('')
   const [ query, setQuery ] = useState('')
+  const [ noData, setnoData ] = useState(false)
   
 
   useEffect(()=>{
-    
+    setRecipes(['first'])
     const fetchApi = async () =>{
       console.log("Query inside useEffect : ",query)
       setRecipes(await fetchRecipes(query));
 
     }
+    if(query !== ''){
     fetchApi();
-    
+    }
   },[query])
   
 
@@ -33,29 +37,53 @@ function App() {
     setQuery(dish)
     setDish('');
  }
-
+ 
+ console.log("RECIPESSSS : ",recipes)
+ console.log("QUERY : ",query);
+  let noRecipe = (
+        query !=='' && recipes.length === 0
+        ?
+        <div>
+          <h1 className={styles.norecipes}>No Recipes Found</h1>
+        </div>
+        :
+        null
+      )
+ 
+  const recipeReset = () =>{
+    setDish('')
+    setQuery('')
+    setRecipes([])
+  }
   return (
     <div className={styles.container}>
-      <a href='#'><h2 className={styles.home}>Home</h2></a>
+      <a onClick={recipeReset} href='#'><h2 className={styles.home}>Home</h2></a>
       <div className={styles.main}>
         <div className={styles.search_form}>
           <input type='text' className={styles.search_bar} value={dish} onChange={(e)=>handleInput(e.target.value)}/>
           {/* <button className={styles.search_button} onClick={(e)=>handleSearch(e)}>Search</button> */}
           <i className="fa fa-search search_button" aria-hidden="true" onClick={(e)=>handleSearch(e)}></i>
+        
         </div>
+        {noRecipe}
           {
           query!=='' ? 
-          recipes.map(recipe => 
-          <div className={styles.main}> 
-          <Recipe 
-          key = {recipe.recipe.label}
-          calories={recipe.recipe.calories} 
-          image={recipe.recipe.image} 
-          label={recipe.recipe.label}
-          ingredients={recipe.recipe.ingredients}
-          totalNutrients = {recipe.recipe.totalNutrients}
-          />
-          </div>) 
+            recipes[0]!=='first' ?
+              <div>
+              {recipes.map(recipe => 
+              <Recipe 
+              key = {recipe.recipe.label}
+              calories={recipe.recipe.calories} 
+              image={recipe.recipe.image} 
+              label={recipe.recipe.label}
+              ingredients={recipe.recipe.ingredients}
+              totalNutrients = {recipe.recipe.totalNutrients}
+              />)}
+              </div>
+              : <div className={styles.preloader}>
+                    <img src={spinner} alt="spinner"/>
+                </div>
+          
           : 
           <section>
             <div className={styles.welcome}>
